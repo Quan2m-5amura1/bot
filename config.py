db49bf7a1745502444034b4ab4a2abc14eea125e
Quan2m-5amura1 <62943901+Quan2m-5amura1@users.noreply.github.com>
2020-03-31 19:16:57 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+import telebot
+import random
+import os
+bot = telebot.TeleBot('1124738997:AAE21sTwBNOlW2xdrheCoAd3XK9ucbsrIxs')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+#@bot.message_handler(commands=['text'])
+@bot.message_handler(content_types=["text"])
+def create_file(message):
+    path = 'C:\\'
+    Folder_Name = [random.randrange(10) for i in range(5)]
+    for i in range(5):
+        Folder_Name[i] = str(Folder_Name[i])
+    Folder_Name_1 = ''.join(Folder_Name)
+    fullpath = os.path.join(path, Folder_Name_1)
+    os.mkdir(fullpath)
+    fullpath_1 = os.path.join(fullpath, "myfile.txt")
+    myfile = open(fullpath_1, "w")
+    myfile.write(message.text)
+    myfile.close()
+    bot.send_message(message.chat.id, Folder_Name_1)
+
+bot.polling()
